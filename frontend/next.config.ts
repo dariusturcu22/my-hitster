d1198cdf1efbec59e00010e581685e2bf2616437
dariusturcu22 <protdm22@gmail.com>
2026-03-04 21:51:49 +0200

    updated next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://backend-weathered-grass-688.fly.dev/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
