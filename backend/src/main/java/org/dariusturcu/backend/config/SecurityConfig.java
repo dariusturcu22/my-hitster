2784a31db25c86b6f04c9551b987c5c33dff9d95
dariusturcu22 <protdm22@gmail.com>
2026-03-10 14:00:40 +0200

    updated configs

--- backend/src/main/java/org/dariusturcu/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/dariusturcu/backend/config/SecurityConfig.java
@@ -100,7 +100,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://my-hitster.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://my-hitster.dariusturcu22.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "https://api.dariusturcu22.com/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
